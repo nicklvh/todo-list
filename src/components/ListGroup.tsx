5dccea1b4350ed625ffe622608d3ab92f82a6c6b
nicklvh <62725534+nicklvh@users.noreply.github.com>
2025-01-01 22:19:47 +0000

    fix: handle checked items better

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -17,6 +17,12 @@ function ListGroup({ items, onClick }: ListGroupProps) {
     window.localStorage.setItem("TO_DO_LIST_CHECKED_ITEMS", JSON.stringify(checkedItems));
   }, [checkedItems]);
 
+  checkedItems.forEach((item) => {
+    if (!items.includes(item)) {
+      setCheckedItems(checkedItems.filter(i => i !== item));
+    }
+  });
+
   const handleCheck = (checked: CheckedState, item: string) => {
     if (!checked && checkedItems.includes(item)) {
       setCheckedItems(checkedItems.filter(i => i !== item));
@@ -29,7 +35,7 @@ function ListGroup({ items, onClick }: ListGroupProps) {
     <div className="flex flex-col items-center justify-between mb-4">
       {items.map((item, index) => {
         return (
-          <div className="flex justify-between w-full">
+          <div className="flex justify-between w-full" key={index}>
             <div className="flex items-center space-x-2">
               <Checkbox checked={checkedItems.includes(item)} onCheckedChange={(checked) => handleCheck(checked, item)} id={`item-${index}`} />
               <label htmlFor={`item-${index}`} className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">{checkedItems.includes(item) ? <s>{item}</s> : <p>{item}</p>}</label>
